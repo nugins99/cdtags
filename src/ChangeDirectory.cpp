e361a5f958a77c589b05546e698065ec50d5c79e
Joe Davidson <jdavidson42@gatech.edu>
2021-04-03 07:18:34 -0400

    Add completion for paths relative to a tag.

--- src/ChangeDirectory.cpp
+++ src/ChangeDirectory.cpp
@@ -1,5 +1,6 @@
 #include "ChangeDirectory.h"
 #include <cstdlib>
+#include "Debug.h"
 
 namespace cdtags {
 
@@ -10,19 +11,55 @@ ChangeDirectory::process(const std::vector<std::string>& args)
   if (args.size() >= 1) {
     // Reverting to previous directory.
     if (args[0] == "-") {
+      DEBUG("Returning to old PWD");
       std::cout << getenv("OLDPWD") << std::endl;
       return 0;
     }
 
-    // Search through the list of tags to find the path.
-    auto it = cfg.paths.find(args[0]);
-    if (it != cfg.paths.end()) {
-      // Found a match - return the full path.
-      std::cout << it->second.string() << std::endl;
-    } else {
-      // No match, just return what was sent to us.
+    // Absolute path or path leading with '.' -> no replacement
+    // Examples:
+    // /home/joe
+    // ../joe
+    // ./Documents/...
+    // or some other relative directory that exists.
+    if (args[0][0] == '/' || args[0][0] == '.' || fs::is_directory(args[0]))
+    {
+      DEBUG("Not replacing path on abs, rel, or existing path");
       std::cout << args[0] << std::endl;
+      return 0;
     }
+
+
+    // Ok.. now we have:
+    // a
+    // a/b
+    // but neither will resolve to a directory, so we attempt to replace it.
+
+    auto possibleTag = fs::path(args[0]).begin()->string();
+    auto resolvedTagIt = cfg.paths.find(possibleTag);
+
+    // Nope - not a tag.
+    if (resolvedTagIt == cfg.paths.end())
+    {
+      DEBUG("Not a tag: " << possibleTag);
+      std::cout << args[0] << std::endl;
+      return 0;
+    }
+
+    // First element matched a tag, replace the tag with the new path.
+    DEBUG("Is a tag: " << possibleTag << " -> " << resolvedTagIt->second);
+
+    // Probably not the most efficient way - but it works.
+    // Maybe we can use ranges?
+    auto path = resolvedTagIt->second;
+    auto taggedPath = fs::path(args[0]);
+    auto taggedPathIt = taggedPath.begin();
+    for(++taggedPathIt; taggedPathIt != taggedPath.end(); ++taggedPathIt)
+    {
+      DEBUG("Appending: " << *taggedPathIt);
+      path /= *taggedPathIt;
+    }
+    std::cout << path.string() << std::endl;
   }
   return 0;
 }
